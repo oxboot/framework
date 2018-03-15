9a094bf4fd540ac3caf16f3213a3eff6465db8c5
Zorca <vs@zorca.org>
2018-03-15 23:09:52 +0300

    Aliases added for app function

--- src/helpers.php
+++ src/helpers.php
@@ -1,11 +1,28 @@
 <?php
 
+use Oxboot\Framework\Application;
+
+if (! function_exists('app')) {
+    function app($abstract = null, array $parameters = [])
+    {
+        $application = Application::getInstance();
+        if (is_null($abstract)) {
+            return $application;
+        }
+        return $application->make($abstract, $parameters);
+    }
+}
+
+if (! function_exists('container')) {
+    function container($abstract = null, array $parameters = [])
+    {
+        return app($abstract, $parameters);
+    }
+}
+
 if (! function_exists('oxboot')) {
     function oxboot($abstract = null, array $parameters = [])
     {
-        if (is_null($abstract)) {
-            return \Oxboot\Framework\Application::getInstance();
-        }
-        return \Oxboot\Framework\Application::getInstance()->make($abstract, $parameters);
+        return app($abstract, $parameters);
     }
 }
